40861a9939a6772a509cbbda02eea43c902114d0
MIT SDE Team IT <153487731+mitsde2023@users.noreply.github.com>
2024-06-07 14:24:38 +0530

    Update PrivateRoute.jsx

--- tmsfru/src/PrivateRoute.jsx
+++ tmsfru/src/PrivateRoute.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "./Context/AuthContext";
+// import { useAuth } from "./Context/AuthContext";
 import Sidebar from "./LayOut/Sidebar";
 import Header from "./LayOut/Header";
 const PrivateRoute = () => {
   const { isAuthenticated } = useAuth();
-  console.log(isAuthenticated, 8);
+  // console.log(isAuthenticated, 8);
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
